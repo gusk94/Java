f560a1be4a64128ae5866f4cea60c0d9be338898
gusk94 <h3652k@gmail.com>
2020-10-21 18:24:24 +0900

    10/21 java study

---
+++ src/programmers/coutnpandy.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class coutnpandy {
+    public static boolean solution(String s){
+        int count_p = 0;
+        int count_y = 0;
+        for (int i=0;i<s.length();i++){
+            if (s.charAt(i) == 'p' || s.charAt(i) == 'P'){
+                count_p++;
+            }else if (s.charAt(i) == 'y' || s.charAt(i) == 'Y'){
+                count_y++;
+            }
+        }
+        if (count_p == count_y){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        String s = "pPoooyY";
+        System.out.println(solution(s));
+    }
+}
